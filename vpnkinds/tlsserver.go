2abdee0e5bc10c4104f2d733c7b54187530d1bc3
Evan2698 <zwh2698@163.com>
2023-10-12 23:46:46 +0800

    update

--- vpnkinds/tlsserver.go
+++ vpnkinds/tlsserver.go
@@ -31,28 +31,3 @@ func runTlsServer(config configure.AppConfig) error {
 	err = tlsServer.ListenAndServeTLS()
 	return err
 }
-
-func runHttp3Client(config configure.AppConfig) error {
-	tlsClient, err := proxycore.NewHttp3Client(config)
-	if err != nil {
-		log.Println("create http3 client failed", err)
-		return err
-	}
-	if tlsClient == nil {
-		return errors.New("create client failed")
-	}
-	defer tlsClient.Close()
-
-	err = tlsClient.RunClient()
-	return err
-}
-
-func runHttp3Server(config configure.AppConfig) error {
-	tlsServer, err := proxycore.NewHttp3Server(config)
-	if err != nil {
-		log.Println("create tls server failed", err)
-		return err
-	}
-	err = tlsServer.ListenAndServeTLS()
-	return err
-}
